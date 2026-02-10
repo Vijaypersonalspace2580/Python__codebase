17832d3cf0c9eea8e4e8ed84cb0c8d39a24f39e6
VIJAY KUMAR REDDY CHINTHAM <vijaykumarreddychintham735@gmail.com>
2026-02-10 17:33:07 +0530

    Refactor contact book functions and add update option
    Refactor contact management functions to improve input validation and add update functionality.

--- contact_book.py
+++ contact_book.py
@@ -1,83 +1,159 @@
 try:
+    phone_book = {}
+    swapped_phone_book = {}
     def add_contacts():
-        name = str(input('Enter name: '))
-        name=name.lower()
-        phone_number = int(input('Enter phone number: '))
-        l = len(str(phone_number))
-        if l == 10:
-            repeat_contact = 0
-            for i, j in phone_book.items():
-                if i == name or j == phone_number:
-                    repeat_contact += 1
-            if repeat_contact == 0:
-                phone_book[name]=phone_number
-                swapped_phone_book[phone_number]=name
+        while True:
+            name = input('Enter name: ')
+            name = name.lower()
+            phone_number = input('Enter phone number: ')
+            if len(phone_number) == 10 and phone_number.isdigit()==True:
+                if name not in phone_book and phone_number not in swapped_phone_book:
+                    phone_book[name] = phone_number
+                    swapped_phone_book[phone_number] = name
+                else:
+                    print('contact is exist')
+                    continue
             else:
-                print('Contact existed')
-                add_contacts()
-        else:
-            print('Invalid phone number')
-            add_contacts()
+                print('Invalid phone number')
+                continue
+            break
     def search_contact():
-        choice = str(input('do you search by name or phone number(n/p): '))
-        if choice == 'n':
-            name = str(input('Enter name: '))
-            name = name.lower()
-            print(phone_book[name])
-        elif choice == 'p':
-            phone_number = str(input('Enter phone number: '))
-            if len(str(phone_number)) == 10:
-                print(swapped_phone_book[phone_number])
+        while True:
+            choice = str(input('do you search by name or phone number(n/p): '))
+            if choice == 'n':
+                while True:
+                    name = input('Enter name: ')
+                    name = name.lower()
+                    if name in phone_book:
+                        print(name, phone_book[name])
+                    else:
+                        print("Contact not found")
+                        continue
+                    break
+            elif choice == 'p':
+                while True:
+                    phone_number = input('Enter phone number: ')
+                    if len(phone_number) == 10 and phone_number.isdigit() == True:
+                        if phone_number in swapped_phone_book:
+                            print(phone_number, swapped_phone_book[phone_number])
+                        else:
+                            print('phone number not found')
+                            continue
+                    else:
+                        print('Invalid phone number')
+                        continue
+                    break
             else:
-                print('Invalid phone number')
-                search_contact()
-        else:
-            print('Invalid option')
-            search_contact()
+                print("Invalid choice")
+                continue
+            break
     def delete_contact():
-        choice = str(input('do you delete by name or phone number(n/p): '))
-        if choice == 'n':
-            name = str(input('Enter name: '))
-            name = name.lower()
-            del phone_book[name]
-        elif choice == 'p':
-            phone_number = str(input('Enter phone number: '))
-            if len(str(phone_number)) == 10:
-                del swapped_phone_book[phone_number]
+        while True:
+            choice = str(input('do you delete by name or phone number(n/p): '))
+            if choice == 'n':
+                while True:
+                    name = input('Enter name: ')
+                    name = name.lower()
+                    if name in phone_book:
+                        phone = phone_book[name]
+                        del phone_book[name]
+                        del swapped_phone_book[phone]
+                        print('Contact deleted successfully')
+                    else:
+                        print('Contact not found')
+                        continue
+                    break
+            elif choice == 'p':
+                while True:
+                    phone_number = input('Enter phone number: ')
+                    if len(phone_number) == 10 and phone_number.isdigit() == True:
+                        if phone_number in swapped_phone_book:
+                            na = swapped_phone_book[phone_number]
+                            del swapped_phone_book[phone_number]
+                            del phone_book[na]
+                            print('Contact deleted successfully')
+                        else:
+                            print('Contact not found')
+                            continue
+                    else:
+                        print('Invalid phone number')
+                        continue
+                    break
             else:
-                print('Invalid phone number')
-                delete_contact()
-        else:
-            print('Invalid option')
-            delete_contact()
-        display_contacts()
+                print("Invalid choice")
+                continue
+            break
+    def update_contact():
+        while True:
+            choice = str(input('Do you update Name or Phone number(n/p): '))
+            if choice == 'n':
+                while True:
+                    old_name = input('Enter old name: ')
+                    old_name = old_name.lower()
+                    if old_name in phone_book:
+                        new_name = input('Enter new name: ')
+                        new_name = new_name.lower()
+                        phone=phone_book[old_name]
+                        swapped_phone_book[phone] = new_name
+                        del phone_book[old_name]
+                        phone_book[new_name] = phone
+                        print('Contact updated successfully')
+                    else:
+                        print('Contact not found')
+                        continue
+                    break
+            elif choice == 'p':
+                while True:
+                    old_phone = input('Enter old phone number: ')
+                    new_phone = input('Enter new phone number: ')
+                    if (len(old_phone) == 10 and old_phone.isdigit()) and (len(new_phone)==10 and new_phone.isdigit()==True)==True:
+                        if old_phone in swapped_phone_book:
+                            nam=swapped_phone_book[old_phone]
+                            phone_book[nam]=new_phone
+                            del swapped_phone_book[old_phone]
+                            swapped_phone_book[new_phone] = nam
+                        else:
+                            print('Contact not found')
+                            continue
+                    else:
+                        print('Invalid phone number')
+                        continue
+                    break
+            else:
+                print('Invalid choice')
+                continue
+            break
     def display_contacts():
-        for i,j in phone_book.items():
-            print(i,j)
-    phone_book={}
-    swapped_phone_book={}
-    n=int(input('Enter how many contacts do you add: '))
+        for k,v in phone_book.items():
+            print(k,v)
+    n = int(input('Enter how many contacts do you add: '))
     print('Enter name and phone number:')
     for i in range(n):
         add_contacts()
-    swapped_phone_book={}
-    s='y'
-    while(s=='y' or s=='Y'):
+    s = 'y'
+    while (s == 'y' or s == 'Y'):
         print('1.Add contact')
         print('2.Search contact')
         print('3.Delete contact')
         print('4.Display all contacts')
-        option=int(input('Enter your option(1,2,3,4): '))
-        if option==1:
+        print('5.Update contact')
+        print('6.Exit')
+        option = int(input('Enter your option(1,2,3,4,5,6): '))
+        if option == 1:
             add_contacts()
-        elif option==2:
+        elif option == 2:
             search_contact()
-        elif option==3:
+        elif option == 3:
             delete_contact()
-        elif option==4:
+        elif option == 4:
             display_contacts()
+        elif option == 5:
+            update_contact()
+        elif option == 6:
+            break
         else:
             print('Invalid option')
-        s=str(input("Do you want choose any other service(y/n)="))
-except:
+            continue
+        s=str(input('Do yo want to choose another service(y/n): '))
+except ValueError:
     print('Invalid input')
